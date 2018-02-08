18bcc88872232a080b9633d405d3c801572fe07b
firearthur <thefirearthur@gmail.com>
2018-02-07 20:00:02 -0800

    alomst done with the helpers

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,9 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  archiveIndex : path.join(__dirname, '../web/public/index.html'),
+  archiveStyles : path.join(__dirname, '../web/public/styles.css')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -26,16 +28,67 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+    fs.readFile(exports.paths.list, 'utf8', function(err, contents) {
+    callback(contents.split('\n'));
+  });
+  
 };
 
 exports.isUrlInList = function(url, callback) {
+
+  fs.readFile(exports.paths.list, 'utf8', function(err, contents) {
+    callback(contents.split('\n').includes(url));
+  });
+  
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, url + "\n", callback)
+  
 };
 
 exports.isUrlArchived = function(url, callback) {
+  var fixturePath = exports.paths.archivedSites;
+  var listOfSiteFiles;
+  fs.readdir(fixturePath, function(err, files){
+  
+    if(err){
+      console.log(err);
+    }
+    callback(files.includes(url));    
+  } );
 };
 
 exports.downloadUrls = function(urls) {
+  let savingPath = exports.archivedSites;
+  if(urls){
+    urls.forEach((url)=>{
+      
+      var fd = fs.openSync(savingPath, 'w');
+      fs.writeSync(fd, url);
+      fs.closeSync(fd);  
+    });
+  }
+
 };
+
+exports.sendResponseFromFile = function(filePath, response){
+  let returnValue = true;
+  fs.readFile(filePath, 'utf8', function(err, contents) {
+    response.end(contents);
+    returnValue = contents || err;
+  });
+
+  return returnValue;   
+}
+
+// const callback = (err, data) => {
+//   if(err) {
+//     res.send(500);
+//   } else {
+//     res.send(201);
+//   }
+// }
+
+// callback(err);
+// callback();
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,56 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
+var httpHealpers = require('./http-helpers');
+
+// var router = {'/': };
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  if(req.method === 'GET'){ //if its a get request
+    if(req.url === '/'){
+      archive.sendResponseFromFile(archive.paths.archiveIndex, res);
+    } else {
+      if(req.url === '/styles.css'){ 
+        archive.sendResponseFromFile(archive.paths.archiveStyles, res);
+      } else {
+
+        var site = req.url.slice(1);
+        archive.isUrlArchived(site, (isIncluded)=>{
+          //console.log(isIncluded)
+          if(isIncluded){
+          archive.sendResponseFromFile(archive.paths.archivedSites + "/" + site, res);
+          } else {
+            res.writeHead(404 , httpHealpers.headers);
+            res.end('file not found');
+          }
+        });
+
+
+      }
+
+   
+    }
+  } else if(req.method === 'POST'){ //if its a post request
+    
+
+    var data = '';
+    req.on('data', function(chunk) {
+
+      data += chunk;
+    });
+    req.on('end', function() {
+     var useless = function (x){return null;};
+      archive.addUrlToList(data.slice(4),useless);
+      res.writeHead(302, httpHealpers.headers);
+      res.end()
+    });
+
+  }
+  
+   
+
+
+  //res.end(archive.paths.archiveIndex);
 };
